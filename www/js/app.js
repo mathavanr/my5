2ebe2786eed34757ea096eecc4c9516eeb22dcb1
mathavanr <mathavan.r94@gmail.com>
2015-03-24 21:30:51 +0530

    another

---
+++ www/js/app.js
@@ -0,0 +1,40 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+angular.module('ionicApp', ['ionic'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+    .state('app', {
+      url: "/app",
+      abstract: true,
+      templateUrl: "app/layout/menu.html",
+      controller: 'AppCtrl'
+    })
+    .state('app.splash', {
+      url: '/',
+      templateUrl: 'splash.html',
+      controller: 'SplashController'
+    })
+    .controller('AppCtrl',function($scope){
+    })
+$urlRouterProvider.otherwise('/app/layout');
+});
